2ad85945a52f8a485550e9f80ad347df9831db0b
sanjucs12 <sanjucs12@gmail.com>
2023-05-15 14:51:02 +0530

    to deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LazyLoadingComponent from "./LazyLoadingComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Lazy Loading</h1>
+      <LazyLoadingComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LazyLoadingComponent.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const LazyLoadingComponent = () => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
+      );
+
+      setResults((prevResults) => [...prevResults, ...response.data]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      fetchData();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          <h3>{result.title}</h3>
+          <p>{result.body}</p>
+        </div>
+      ))}
+      {loading && <p>Loading...</p>}
+    </div>
+  );
+};
+
+export default LazyLoadingComponent;
